90c9a2ff970e3e89440d3e3b29399408151b8503
red-ait <redouanmkdem@gmail.com>
2021-03-23 16:35:20 +0100

    configure ask for storage permission

--- app/src/main/java/fr/isima/technomobile/activities/DetailDepenseActivity.java
+++ app/src/main/java/fr/isima/technomobile/activities/DetailDepenseActivity.java
@@ -4,8 +4,11 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +18,7 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ajts.androidmads.library.SQLiteToExcel;
 
@@ -42,6 +46,7 @@ public class DetailDepenseActivity extends AppCompatActivity {
     private static final String TAG = "LOG_INF";
     private Depenses selectedDepense;
     ArrayList<DetailDepense> detailDepenses = new ArrayList<>();
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +70,9 @@ public class DetailDepenseActivity extends AppCompatActivity {
         recap.setOnClickListener(v -> {
             sendRecap(selectedDepense);
         });
+        if (!checkPermission()) {
+            requestPermission(); // Code for permission
+        }
     }
     public static DetailDepenseActivity getInstanceActivity() {
         return weakActivity.get();
@@ -101,6 +109,37 @@ public class DetailDepenseActivity extends AppCompatActivity {
         TextView depSum = findViewById(R.id.depense_sum);
         depSum.setText(formatter.format(depenseSum) + " EUR");
 
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(DetailDepenseActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(DetailDepenseActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(DetailDepenseActivity.this, "Write External Storage permission allows us to do export data. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(DetailDepenseActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("value", "Permission Granted, Now you can use local drive .");
+                } else {
+                    Log.e("value", "Permission Denied, You cannot use local drive .");
+                }
+                break;
+        }
     }
     public void updateDetailDepenseList() {
         MemberDBHelper memberDBHelper = new MemberDBHelper(this);
